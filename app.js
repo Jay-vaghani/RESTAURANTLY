950fad3f22e74b1ae8a9f091e8ae0deb53534654
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2023-03-30 12:09:45 +0530

    section 7

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 let swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  breakpoints: {
+
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 30,
     },
-    loop: true,
-  });
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
